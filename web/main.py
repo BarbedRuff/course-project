b5f252b21fbb282d1367b68c472608becfa8786a
Eilzaveta <eilzaveta@MacBook-Air-Eilzaveta.local>
2023-12-17 20:15:23 +0300

    update

--- web/main.py
+++ web/main.py
@@ -7,10 +7,19 @@ app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
 
 
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "id": "1"})
+
+
+@app.get("/bad", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("bad.html", {"request": request, "id": "1"})
+
+
+@app.get("/good", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("good.html", {"request": request, "id": "1"})
